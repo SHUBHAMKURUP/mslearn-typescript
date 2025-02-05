dd60c4f7def5b19a434b70a08a454b4124c452f3
shubham <shubham3325@gmail.com>
2025-02-05 11:56:58 +0530

    Refactor DataStore class to support generics for storing items of any type

--- code/module-06/m06-start/module06.js
+++ code/module-06/m06-start/module06.js
@@ -1,51 +1,43 @@
 /*  Module 6: DGenerics in TypeScript
     Lab Start */
-
-/*  DataStore is a utility function that can store up to 10 string values in an array. 
+/*  DataStore is a utility function that can store up to 10 string values in an array.
     Rewrite the DataStore class so the array can store items of any type.
 
     TODO: Add and apply a type variable. */
-class DataStore<T> {
-  private _data: Array<T> = new Array(10);
-
-  AddOrUpdate(index: number, item: T) {
-    if (index >= 0 && index < 10) {
-      this._data[index] = item;
-    } else {
+var DataStore = /** @class */ (function () {
+    function DataStore() {
+        this._data = new Array(10);
     }
-  }
-  GetData(index: number) {
-    if (index >= 0 && index < 10) {
-      return this._data[index];
-    } else {
-      return;
-    }
-  }
-}
-
-let cities = new DataStore<string>();
-
+    DataStore.prototype.AddOrUpdate = function (index, item) {
+        if (index >= 0 && index < 10) {
+            this._data[index] = item;
+        }
+        else {
+        }
+    };
+    DataStore.prototype.GetData = function (index) {
+        if (index >= 0 && index < 10) {
+            return this._data[index];
+        }
+        else {
+            return;
+        }
+    };
+    return DataStore;
+}());
+var cities = new DataStore();
 cities.AddOrUpdate(0, "Mumbai");
 cities.AddOrUpdate(1, "Chicago");
 cities.AddOrUpdate(11, "London"); // item not added
-
 console.log(cities.GetData(1)); // returns 'Chicago'
 console.log(cities.GetData(12)); // returns 'undefined'
-
 // TODO Test items as numbers.
-let empIDs = new DataStore<number>();
+var empIDs = new DataStore();
 empIDs.AddOrUpdate(0, 50);
 empIDs.AddOrUpdate(1, 65);
 empIDs.AddOrUpdate(2, 89);
 console.log(empIDs.GetData(0));
-
-// TODO Test items as objects.
-type Pets = {
-  name: string;
-  breed: string;
-  age: number;
-};
-let pets = new DataStore<Pets>();
+var pets = new DataStore();
 pets.AddOrUpdate(0, { name: "Rex", breed: "Golden Retriever", age: 5 });
 pets.AddOrUpdate(1, { name: "Sparky", breed: "Jack Russell Terrier", age: 3 });
 console.log(pets.GetData(1));
